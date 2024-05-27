e7b2cf65480295e4f2dbe7c0676481212590010e
Felipe Santos <jfcardososantos@MacBook-Air-M1-2020-de-Felipe.local>
2024-05-27 16:02:55 -0300

    loginComplete

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,11 +1,15 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: 'AIzaSyBx2DPPVMo3DeiXX8zKlaOqVYgAeDG8Wco',
-    authDomain: 'buteco-b6074.firebaseapp.com',
-    projectId: 'buteco-b6074',
-    storageBucket: 'buteco-b6074.appspot.com',
-    messagingSenderId: '570428800396',
-    appId: '1:570428800396:web:3062b511819ec4d529aaa9',
+    apiKey: "AIzaSyDcnq5td5bWBaTxE8J98RVx4rbRcDgwzFU",
+    authDomain: "boteco-dev.firebaseapp.com",
+    databaseURL: "https://boteco-dev-default-rtdb.firebaseio.com",
+    projectId: "boteco-dev",
+    storageBucket: "boteco-dev.appspot.com",
+    messagingSenderId: "906209299021",
+    appId: "1:906209299021:web:a2bac47b7269d1bd2e2a04"
   },
+
+  barName: "BAR DO LEOMI",
+  barLogo: "https://s3-sa-east-1.amazonaws.com/projetos-artes/fullsize%2f2019%2f08%2f02%2f13%2fLogo-259466_341712_132556404_1090813604.jpg"
 };
